ce25b8a657b90db5689c9f3b0128e2f72027eaa8
Raksha Ramteke <rramteke@deqode.com>
2021-07-10 21:57:59 +0530

    Minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_150521) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.published_at
   end
 
   create_table "users", force: :cascade do |t|
