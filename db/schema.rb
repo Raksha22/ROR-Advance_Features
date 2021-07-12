2657aee7756e6a64ecd0767bcbe223a0fdd91a5c
Raksha Ramteke <rramteke@deqode.com>
2021-07-12 22:21:00 +0530

    Removed unused files

--- db/schema.rb
+++ db/schema.rb
@@ -29,14 +29,6 @@ ActiveRecord::Schema.define(version: 2021_07_12_120409) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
-  create_table "admin_roles", force: :cascade do |t|
-    t.string "role"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "admin_user_id"
-    t.index ["admin_user_id"], name: "index_admin_roles_on_admin_user_id"
-  end
-
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
