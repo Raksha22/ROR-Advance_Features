8e6b98a798a4de4d3e04d9e4afdb44d5b3ff0359
Raksha Ramteke <rramteke@deqode.com>
2021-07-16 14:04:20 +0530

    Modified post column type

--- db/migrate/20210715142726_add_column_to_post.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToPost < ActiveRecord::Migration[6.1]
-  def change
-    add_column :posts, :rating, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_15_142726) do
+ActiveRecord::Schema.define(version: 2021_07_16_081921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2021_07_15_142726) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "published"
-    t.integer "rating"
+    t.float "rating"
   end
 
   create_table "users", force: :cascade do |t|
