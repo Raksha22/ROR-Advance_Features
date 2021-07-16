6d95f02e8f8e98e680e42431c0f2faf4c931e302
Raksha Ramteke <rramteke@deqode.com>
2021-07-16 15:18:07 +0000

    Fixed indentations

--- app/interactions/admin_users/create_admin_user.rb
+++ app/interactions/admin_users/create_admin_user.rb
@@ -1,11 +1,11 @@
 module AdminUsers
 
-	class CreateAdminUser < ActiveInteraction::Base
-		string :email, :name, :admin_role_id, :password
+  class CreateAdminUser < ActiveInteraction::Base
+	string :email, :name, :admin_role_id, :password
 
-		def execute
-		  admin_user = AdminUser.create!(email: email, name: name, admin_role_id: admin_role_id, password: password)
-			admin_user
-		end
+	def execute
+	  admin_user = AdminUser.create!(email: email, name: name, admin_role_id: admin_role_id, password: password)
+	  admin_user
 	end
+  end
 end
--- app/interactions/admin_users/update_admin_user.rb
+++ app/interactions/admin_users/update_admin_user.rb
@@ -1,11 +1,11 @@
 module AdminUsers
 
-	class UpdateAdminUser < ActiveInteraction::Base
-		string :email, :name, :admin_role_id, :password
+  class UpdateAdminUser < ActiveInteraction::Base
+	string :email, :name, :admin_role_id, :password
 
-		def execute
-		  admin_user = AdminUser.update(email: email, name: name, admin_role_id: admin_role_id, password: password)
-		  admin_user
-		end
+	def execute
+	  admin_user = AdminUser.update(email: email, name: name, admin_role_id: admin_role_id, password: password)
+	  admin_user
 	end
+  end
 end
