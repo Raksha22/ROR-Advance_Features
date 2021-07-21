08db7c9a701e2877d52a84975e21d5ed19a4cef1
Raksha Ramteke <rramteke@deqode.com>
2021-07-21 15:26:49 +0530

    Fetch data by URL

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -3,5 +3,13 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  def scrape_city_urls
+    url = 'http://www.museumsusa.org/museums/?k=1271400%2cState%3aID%3bDirectoryID%3a200454'
+    doc = Nokogiri::HTML(URI.open(url))
 
+    cities = doc.css('#city').css('.browseCategoryItem')
+  end
 end
+
+scrape = Scraper.new
+scrape.scrape_city_urls
