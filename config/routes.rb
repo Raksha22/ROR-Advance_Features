e3469bb269ee25cfa5c93465a4405d78fcaa6653
Raksha Ramteke <rramteke@deqode.com>
2021-07-23 18:39:15 +0530

    Add basic UI for has_many through association

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root 'employees#index'
+  root 'tutors#index'
   devise_for :users
   resources :users
   resources :employees, only: [:index]
+  resources :tutors
+  resources :students
 
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
