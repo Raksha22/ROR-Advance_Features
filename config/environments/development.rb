1e7ac0e0c675f314293159623b47e05dc8141ce9
Raksha Ramteke <rramteke@deqode.com>
2021-07-26 19:16:07 +0530

    Modified figaro variable name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'], port: 3000 }
+  config.action_mailer.default_url_options = { host: Figaro.env.MAIL_HOST, port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
